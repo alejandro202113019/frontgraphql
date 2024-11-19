bf8f8577dbac1ab680b49fd88b950af44b44a741
CamiloCorrea01 <38996951+CamiloCorrea01@users.noreply.github.com>
2024-11-19 14:38:11 -0500

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -17,7 +17,7 @@ const Login = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://taller1-2do50-electiva2.onrender.com/api', {
+      const response = await fetch('https://taller1-2do50-electiva2.onrender.com', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
